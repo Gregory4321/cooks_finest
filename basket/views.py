b35bb6fe0a1c6002b3b5cc9fbe1a4dcb8be547de
Gregory4321 <greg-lewis@hotmail.co.uk>
2021-04-08 13:34:56 +0000

    Added basket app, urls and template

---
+++ basket/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def view_basket(request):
+    """
+    A view that renders the basket contents page
+    """
+
+    return render(request, 'basket/basket.html')
