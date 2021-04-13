c2313d82c6f7bd3b59c8538ba034a773c6203970
Gregory4321 <greg-lewis@hotmail.co.uk>
2021-04-13 15:51:58 +0000

    Added checkout success logic. Fixed model typo and migrated

--- checkout/models.py
+++ checkout/models.py
@@ -16,7 +16,7 @@ class Order(models.Model):
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     promotion_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
@@ -34,7 +34,7 @@ class Order(models.Model):
         Update the grand total each time a line item is added,
         accounting for promotional discounts
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total > settings.DISCOUNT_THRESHOLD:
             self.promotion_cost = self.order_total * settings.STANDARD_PROMOTION_PERCENTAGE / 100
         else:
