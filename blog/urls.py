458a0d4f4c460ab27d0f6a9164f2c86b24cde758
Gregory4321 <greg-lewis@hotmail.co.uk>
2021-04-20 16:05:37 +0000

    Added blog_details views, urls and template. Style with linked css file. Links to details view added to blog_post page

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_blog_posts, name='blogs')
+    path('', views.all_blog_posts, name='blogs'),
+    path('detail/<slug:slug>/', views.blog_detail, name='blog_detail'),
 ]
