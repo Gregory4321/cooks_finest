957ac1685c3af7f2c938328d23c85dc65f20cb77
Gregory4321 <greg-lewis@hotmail.co.uk>
2021-04-22 13:26:37 +0000

    Added new arrivals section to home page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """
     A view to return the index page
     """
+    home_new_arrivals = Product.objects.filter(category__name='new_arrivals')
 
-    return render(request, 'home/index.html')
+    context = {
+        'home_new_arrivals': home_new_arrivals
+    }
+
+    return render(request, 'home/index.html', context)
