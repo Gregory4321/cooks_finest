b17740c50b7f3efbe5740164a72282b846f9e718
Gregory4321 <greg-lewis@hotmail.co.uk>
2021-04-23 12:27:13 +0000

    Added reviews view, url and template

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import ReviewForm
 
-# Create your views here.
+
+def add_review(request):
+    """
+    Add a review to a product
+    """
+
+    form = ReviewForm()
+    template = 'reviews/add_review.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
