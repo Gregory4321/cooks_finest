1379f30d675eccb2794ed4dd1b4e56ae6fe01872
Gregory4321 <greg-lewis@hotmail.co.uk>
2021-04-23 18:46:28 +0000

    Refactoring, error checking and adding comments to checkout app

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import (
+    render, redirect, get_object_or_404
+)
 from django.urls import reverse
 from django.http import HttpResponse
 from django.contrib import messages
