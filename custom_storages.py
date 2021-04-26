48d3757147b4b9546646206a10541c0e87ea67c7
Gregory4321 <greg-lewis@hotmail.co.uk>
2021-04-26 12:31:34 +0000

    Connect django and aws s3 services, creating custom_storages.py

---
+++ custom_storages.py
@@ -0,0 +1,19 @@
+"""
+CustomStorages.py
+"""
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    """
+    Tell where to store static files
+    """
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    """
+    Tell where to store media files
+    """
+    location = settings.MEDIAFILES_LOCATION
