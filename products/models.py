a9b5e8c77e958de8f0a7ea458d684a2cbb1f4684
Gregory4321 <greg-lewis@hotmail.co.uk>
2021-04-26 20:00:18 +0000

    Completed add review functionality

--- products/models.py
+++ products/models.py
@@ -2,15 +2,16 @@
 Product models.py
 """
 from django.db import models
-from django.db.models import Avg
+# from django.db.models import Avg
+# from reviews.models import Review
 
 
-def calculate_rating(self):
-    """
-    Calculate rating from reviews
-    """
-    self.rating = self.reviews.aggregate(Avg("review_rating"))
-    self.save()
+# def calculate_rating(self):
+#     """
+#     Calculate rating from reviews
+#     """
+#     self.rating = self.reviews.aggregate(Avg("review_rating"))
+#     self.save()
 
 
 class Category(models.Model):
@@ -52,8 +53,8 @@ class Product(models.Model):
         decimal_places=2,
         null=True,
         blank=True)
-    image = models.ImageField(default='', blank=True)
     image_url = models.URLField(max_length=1024, default='', blank=True)
+    image = models.ImageField(default='', blank=True)
 
     def __str__(self):
         return self.name
--- reviews/views.py
+++ reviews/views.py
@@ -1,16 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.contrib import messages
+from products.models import Product
 from .forms import ReviewForm
+from .models import Review
+from profiles.models import UserProfile
+from django.db.models import Avg
 
 
-def add_review(request):
+def add_review(request, product_id):
     """
     Add a review to a product
     """
-
-    form = ReviewForm()
+    product = get_object_or_404(Product, pk=product_id)
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review_title = form.cleaned_data['review_title']
+            review_content = form.cleaned_data['review_content']
+            review_rating = form.cleaned_data['review_rating']
+            Review.objects.create(
+                user_profile=user_profile,
+                product=get_object_or_404(Product, pk=product_id),
+                review_title=review_title,
+                review_rating=review_rating,
+                review_content=review_content)
+            messages.success(request, 'Successfully addded review.')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to add product. \
+                    Please check the form is valid and try again.')
+    else:
+        form = ReviewForm()
     template = 'reviews/add_review.html'
     context = {
         'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
